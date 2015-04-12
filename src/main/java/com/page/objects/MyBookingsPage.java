830562569ea160a35b96725520277bd954cb736b
thachbui88 <buinguyenthach@gmail.com>
2015-04-12 15:02:24 +0700

    Add file MyBookingsPage.java

--- src/main/java/com/page/objects/MyBookingsPage.java
+++ src/main/java/com/page/objects/MyBookingsPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class MyBookingsPage extends GeneralPage {
 
@@ -19,11 +21,32 @@ public class MyBookingsPage extends GeneralPage {
     @FindBy(id = "ctl00_ctl00_MainContent_leftMenu1_lbtSignOut")
     private WebElement signOut;
 
+    @FindBy(xpath = "//*[@id='ctl00_ctl00_MainContent_leftMenu1_udpMenu']//ul//li")
+    private List<WebElement> panelItems;
+
+    @FindBy(xpath = "//*[contains(@class,'header')]//h1")
+    private WebElement textHeader;
+
+    @FindBy(id = "ctl00_ctl00_MainContent_leftMenu1_lbtMyCreditCards")
+    private WebElement linkMyCardDetails;
+
+    @FindBy(id = "password")
+    private WebElement enterPassWord;
+
+    @FindBy(id = "signin-btn")
+    private WebElement signInButton;
+
+    @FindBy(xpath = "//a[.='Sign Out']")
+    private WebElement signOutOnTop;
+
+    @FindBy(id = "ShowAddCardDivButton")
+    private WebElement ShowAddCardDivButton;
+
     public void load() {
         loadPage("/rewards/managebooking.html");
     }
 
-    public void isLoaded(){
+    public void isLoaded() {
         webPageFactory.checkWeAreOnTheRightPage("My Bookings");
     }
 
@@ -32,4 +55,46 @@ public class MyBookingsPage extends GeneralPage {
         return webPageFactory.getLogInPage();
     }
 
+    public boolean shouldDisplayEmailLogin(String text) {
+        String xpath = "//*[contains(@class,'account_email')]";
+        WebElement element = webElementFinder.findElementByLocatorXPath(xpath);
+        return element.getText().equals(text);
+    }
+
+    public String verifyLeftPanelItems(int i) {
+        return panelItems.get(i).getText();
+    }
+
+    public boolean shouldDisplayTextHeader(String text) {
+        return textHeader.getText().equals(text);
+    }
+
+    public MyBookingsPage clickMyCardDetails() {
+        linkMyCardDetails.click();
+        return this;
+    }
+
+    public MyBookingsPage enterPasswordAgian(String password) {
+        enterPassWord.sendKeys(password);
+        return this;
+    }
+
+    public MyBookingsPage clickSignIn() {
+        signInButton.click();
+        return this;
+    }
+
+    public MyBookingsPage confirmLoginAgian(String password) {
+        return enterPasswordAgian(password).clickSignIn();
+    }
+
+    public LogInPage clickSignOutOnTop() {
+        signOutOnTop.click();
+        return webPageFactory.getLogInPage();
+    }
+
+    public boolean verifyShouldDisplayAddNewCardButton() {
+        return ShowAddCardDivButton.isDisplayed();
+    }
 }
+
