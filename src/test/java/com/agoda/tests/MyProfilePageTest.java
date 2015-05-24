5d84ec5e58a1c1bdecee85ff9c9c5617aaee8fbd
anvo86 <anvo86@gmail.com>
2015-05-24 17:14:01 +0700

    An commit MyProfilePageTest.java

--- src/test/java/com/agoda/tests/MyProfilePageTest.java
+++ src/test/java/com/agoda/tests/MyProfilePageTest.java
@@ -3,7 +3,6 @@ package com.agoda.tests;
 import com.page.objects.LogInPage;
 import com.page.objects.MyBookingsPage;
 import com.page.objects.MyProfilePage;
-import com.web.coreframework.Common;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -22,19 +21,29 @@ public class MyProfilePageTest extends BasePage {
     private String COUNTRY;
     private String LANGUAGE;
     private String PHONE;
+    private String COLOR;
+    private String BACKGROUNDCOLOR;
+    private String DATE;
+    private String SUCCESS_NOTIFY;
+    private String EMAIL;
 
     @BeforeClass
     public void beforeClass() {
         EMAIL1 = testData.getProperty("email1");
         EMAIL4 = testData.getProperty("email4");
-        PASSWORD = testData.getProperty("password");
+        PASSWORD = testData.getProperty("password3");
         GENDER = testData.getProperty("gender");
         COUNTRY = testData.getProperty("country");
         LANGUAGE = testData.getProperty("language");
         PHONE = testData.getProperty("phone");
+        COLOR = testData.getProperty("color");
+        BACKGROUNDCOLOR = testData.getProperty("backgroundcolor");
+        DATE = testData.getProperty("date");
+        SUCCESS_NOTIFY = testData.getProperty("notify");
+        EMAIL = testData.getProperty("email");
 
         logInPage = webPageFactory.loadLogInPage();
-        myBookingsPage = logInPage.logInPassed(EMAIL1, PASSWORD);
+        myBookingsPage = logInPage.logInPassed(EMAIL4, PASSWORD);
         myBookingsPage.isLoaded();
         myProfilePage = webPageFactory.getMyProfilePage();
     }
@@ -48,6 +57,7 @@ public class MyProfilePageTest extends BasePage {
      * ******************************************************************************************
      * PERFORMS ALL TEST CASES																	*
      * ******************************************************************************************
+     * //
      */
     @Test(description = "verify Default Page Displayed")
     public void VerifyDefaultPageDisplayed() {
@@ -61,9 +71,9 @@ public class MyProfilePageTest extends BasePage {
     @Test(description = "verify Update Information Profile Incorrect")
     public void verifyUpdateInformationProfileIncorrect() {
         myProfilePage.clickMyProfile()
-                     .clickLinkEditInformation()
-                     .chooseCountryOfPassport("Please Select")
-                     .clickLinkSaveInformation();
+                .clickLinkEditInformation()
+                .chooseCountryOfPassport("Please Select")
+                .clickLinkSaveInformation();
 
         Assert.assertTrue(myProfilePage.shouldSeeTextNotificationError("Basic Information is incorrect."));
     }
@@ -71,38 +81,44 @@ public class MyProfilePageTest extends BasePage {
     @Test(description = "verify Update Information Successfully")
     public void verifyUpdateInformationSuccessfully() {
         myProfilePage.clickMyProfile()
-                     .clickLinkEditInformation()
-                     .editEmail("test_selenium@gmail.com")
+                .clickLinkEditInformation()
+                .editEmail("test_selenium@gmail.com")
 //                   .chooseCountryOfPassport("Vietnam")
-                     .selectTheCheckbox()
-                     .clickLinkSaveInformation();
+                .selectTheCheckbox()
+                .clickLinkSaveInformation();
 
         //verify information has been changed
 //        Assert.assertEquals(myProfilePage.shouldSeeTextCountry(), "Vietnam");
         Assert.assertEquals(myProfilePage.shouldSeeTextBasicInformationUpdateSuccessfully(), "Your Basic Information has been changed successfully.");
-        Assert.assertEquals(myProfilePage.shouldSeeTextEmailEdit(), "test_selenium@gmail.com");
+//        Assert.assertEquals(myProfilePage.shouldSeeTextEmailEdit(), "test_selenium@gmail.com");
         Assert.assertTrue(myProfilePage.shouldSeeTextCheckboxSelected("Business travelers, Couples, Solo travelers"));
 
 //      //Clean up data
         myProfilePage.clickMyProfile()
-                     .clickLinkEditInformation()
-                     .editEmail("testt@gmail.com")
-//                   .chooseCountryOfPassport("United Kingdom")
-                     .deSelectTheCheckbox()
-                     .clickLinkSaveInformation();
-    }
-    @Test
-    public void verifyEditInformationSuccessfully() {
-        myProfilePage.clickMyProfile()
                 .clickLinkEditInformation()
+                .editEmail("testt@gmail.com")
+//                   .chooseCountryOfPassport("United Kingdom")
+                .deSelectTheCheckbox()
+                .clickLinkSaveInformation();
+    }
+    @Test(description = "verify Edit Information Successfully")
+    public void verifyEditInformationSuccessfully() {
+            myProfilePage.shouldClickMyProfile()
+                .shouldClickLinkEditInformation()
                 .shouldChooseGenderName(GENDER)
                 .editEmail(EMAIL4)
-                .shouldChooseDatePicker("5")
-                .chooseCountryOfPassport(COUNTRY)
-                .shouldChooseLanguage(LANGUAGE)
+                .shouldChooseDatePicker("24")
+                .shouldChooseCountryPassport(COUNTRY)
+                .shouldEditLanguage(LANGUAGE)
                 .deSelectTheCheckbox()
                 .shouldSelectTheCheckbox()
                 .shouldEnterMobilePhoneNumber(PHONE)
-                .clickLinkSaveInformation();
+                .clickSaveAllInformation();
+        Assert.assertEquals(myProfilePage.shouldSeeSuccessNotify(), SUCCESS_NOTIFY);
+        Assert.assertEquals(myProfilePage.shouldSeeTextColor(), COLOR);
+        Assert.assertEquals(myProfilePage.shouldSeeBackgroundColor(), BACKGROUNDCOLOR);
+        Assert.assertEquals(myProfilePage.shouldSeeEditedEmail(), EMAIL);
+        Assert.assertEquals(myProfilePage.shouldSeeEditedLanguage(), LANGUAGE);
+        Assert.assertEquals(myProfilePage.shouldSeeCountryPassport(),COUNTRY );
     }
 }
